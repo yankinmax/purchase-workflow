ebe94c696c9a69272e04407b6d4e95df8810e884
Maksym Yankin <yankinmk@gmail.com>
2025-01-06 19:41:36 +0200

    [MIG] purchase_analytic_global: Migration to 18.0

--- purchase_analytic_global/models/purchase.py
+++ purchase_analytic_global/models/purchase.py
@@ -1,7 +1,7 @@
 # Copyright 2014-2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models
+from odoo import Command, api, fields, models
 
 
 class PurchaseOrder(models.Model):
@@ -12,20 +12,22 @@ class PurchaseOrder(models.Model):
         string="Analytic Account",
         compute="_compute_analytic_account",
         inverse="_inverse_analytic_account",
-        help="This account will be propagated to all lines, if you need "
-        "to use different accounts, define the account at line level.",
+        help="This account will be propagated to all lines distributions, if you need "
+        "to use different distribution accounts, define the accounts at line level.",
     )
 
-    @api.depends("order_line.account_analytic_id")
+    @api.depends("order_line.distribution_analytic_account_ids")
     def _compute_analytic_account(self):
         for rec in self:
-            account = rec.mapped("order_line.account_analytic_id")
-            if len(account) == 1:
-                rec.account_analytic_id = account
+            accounts_ids = rec.mapped("order_line.distribution_analytic_account_ids")
+            if len(accounts_ids) == 1:
+                rec.account_analytic_id = accounts_ids
             else:
                 rec.account_analytic_id = False
 
     def _inverse_analytic_account(self):
         for rec in self:
             if rec.account_analytic_id:
-                rec.order_line.account_analytic_id = rec.account_analytic_id
+                rec.order_line.distribution_analytic_account_ids = [
+                    Command.set(rec.account_analytic_id.ids)
+                ]
--- purchase_analytic_global/tests/test_purchase_analytic_global.py
+++ purchase_analytic_global/tests/test_purchase_analytic_global.py
@@ -1,24 +1,30 @@
+# Copyright 2025 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from datetime import date
 
-from odoo.tests.common import Form, TransactionCase
+from odoo import Command
+from odoo.tests.common import TransactionCase
+
+from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
 
 
 class TestPurchaseAnalyticGlobal(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.env = cls.env["base"].with_context(**DISABLED_MAIL_CONTEXT).env
         cls.purchase_order_model = cls.env["purchase.order"]
         cls.partner_model = cls.env["res.partner"]
         cls.analytic_account_model = cls.env["account.analytic.account"]
+        cls.analytic_plan = cls.env["account.analytic.plan"].create({"name": "Plan"})
         cls.partner1 = cls.partner_model.create({"name": "Partner1"})
         cls.partner2 = cls.partner_model.create({"name": "Partner2"})
         cls.analytic_account1 = cls.analytic_account_model.create(
-            {"name": "Analytic Account 1"}
+            {"name": "Analytic Account 1", "plan_id": cls.analytic_plan.id}
         )
         cls.analytic_account2 = cls.analytic_account_model.create(
-            {"name": "Analytic Account 2"}
+            {"name": "Analytic Account 2", "plan_id": cls.analytic_plan.id}
         )
         cls.product = cls.env.ref("product.product_product_4")
         cls.purchase_order1 = cls.purchase_order_model.create(
@@ -26,17 +32,16 @@ class TestPurchaseAnalyticGlobal(TransactionCase):
                 "partner_id": cls.partner1.id,
                 "account_analytic_id": cls.analytic_account1.id,
                 "order_line": [
-                    (
-                        0,
-                        0,
+                    Command.create(
                         {
-                            "product_id": cls.product.id,
+                            "analytic_distribution": {cls.analytic_account1.id: 31},
                             "name": cls.product.name,
+                            "product_id": cls.product.id,
                             "product_qty": 10,
                             "price_unit": 50,
                             "product_uom": cls.product.uom_id.id,
                             "date_planned": date.today(),
-                        },
+                        }
                     )
                 ],
             }
@@ -45,18 +50,16 @@ class TestPurchaseAnalyticGlobal(TransactionCase):
             {
                 "partner_id": cls.partner2.id,
                 "order_line": [
-                    (
-                        0,
-                        0,
+                    Command.create(
                         {
-                            "product_id": cls.product.id,
+                            "analytic_distribution": {cls.analytic_account2.id: 44},
                             "name": cls.product.name,
+                            "product_id": cls.product.id,
                             "product_qty": 5,
                             "price_unit": 40,
-                            "account_analytic_id": cls.analytic_account2.id,
                             "product_uom": cls.product.uom_id.id,
                             "date_planned": date.today(),
-                        },
+                        }
                     )
                 ],
             }
@@ -64,20 +67,35 @@ class TestPurchaseAnalyticGlobal(TransactionCase):
 
     def test_purchase_order_check(self):
         self.assertEqual(
-            self.purchase_order1.order_line[0].account_analytic_id,
+            self.purchase_order1.order_line[0].distribution_analytic_account_ids,
             self.analytic_account1,
+        )
+        self.assertEqual(
+            self.purchase_order1.account_analytic_id,
+            self.analytic_account1,
+        )
+        self.assertEqual(
+            self.purchase_order2.order_line[0].distribution_analytic_account_ids,
+            self.analytic_account2,
         )
         self.assertEqual(
             self.purchase_order2.account_analytic_id, self.analytic_account2
         )
-        purchase_form = Form(self.purchase_order2)
-        with purchase_form.order_line.new() as line_form:
-            line_form.product_id = self.product
-            line_form.name = self.product.name
-            line_form.product_qty = 10
-            line_form.price_unit = 20
-            line_form.account_analytic_id = self.analytic_account1
-            line_form.product_uom = self.product.uom_id
-            line_form.date_planned = date.today()
-        purchase_form.save()
+        self.purchase_order2.write(
+            {
+                "order_line": [
+                    Command.create(
+                        {
+                            "analytic_distribution": {self.analytic_account1.id: 55},
+                            "name": self.product.name,
+                            "product_id": self.product.id,
+                            "product_qty": 10,
+                            "price_unit": 20,
+                            "product_uom": self.product.uom_id.id,
+                            "date_planned": date.today(),
+                        }
+                    )
+                ],
+            }
+        )
         self.assertFalse(self.purchase_order2.account_analytic_id)
